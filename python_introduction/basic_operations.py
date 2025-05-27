183642088603cf6bc826b6ecab99a50ef810f259
mwunnam <mwunnam@gmail.com>
2025-05-27 22:44:55 +0000

    task 0 done

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+# Addition
+add_result = number1 + number2
+
+# Subtraction 
+sub_result = number1 - number2
+
+# Multiplication 
+mul_result = number1 * number2
+
+# Outputs
+print("Addition of {} and {} is {}".format(number1, number2, add_result))
+print("Subtraction of {} and {} is {}".format(number1, number2, sub_result))
+print("Multiplication of {} and {} is {}".format(number1, number2, mul_result))
