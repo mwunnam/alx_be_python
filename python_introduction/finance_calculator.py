4f6baf5732b351a8c6fb6a36959b0d99d09aac9f
mwunnam <mwunnam@gmail.com>
2025-05-27 23:35:02 +0000

    correction task 5

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,11 +3,11 @@ m_income = int(input("Enter your monthly income: "))
 t_expense = int(input("Enter your total monthly expenses: "))
 
 # Calculate Monthly Saving
-m_saving = m_income - t_expense
+m_savings = m_income - t_expense
 
 # Projected Annual Saving
 annual_rate = 0.05
-projected_saving = m_saving * 12 + (m_saving * 12 * annual_rate)\
+projected_saving = m_savings * 12 + (m_savings * 12 * annual_rate)
 
-print("Your monthly savings are ${}.".format(m_saving))
+print("Your monthly savings are ${}.".format(m_savings))
 print("Projected savings after one year, with interest, is: ${:.0f}.".format(projected_saving))
