631dfa3f9cac43d57a9d20478d325cc6aaa52155
mwunnam <mwunnam@gmail.com>
2025-05-28 01:29:16 +0000

    correction task 2

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for x in range(1, 11):
     result = number * x
-    print("{} * {} = {}".format(number,x, result))
+    print(f"{number} * {x} = {number * x}")
