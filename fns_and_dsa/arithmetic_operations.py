c08e06df068e2e0d85aa9b9a020e9e004df7c644
mwunnam <mwunnam@gmail.com>
2025-06-11 17:40:37 +0000

    correction

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
-def perform_operation(num1, num2, operator):
-    match operator:
+def perform_operation(num1, num2, operation):
+    match operation:
         case "add":
             return num1 + num2
 
