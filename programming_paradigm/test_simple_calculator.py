a7883dc6b58dc8645d18c60045537f5814e811f3
mwunnam <mwunnam@gmail.com>
2025-06-26 11:30:19 +0000

    done with task 2

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,30 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 4), 8)
+        self.assertEqual(self.calc.multiply(0, 4), 0)
+        self.assertEqual(self.calc.multiply(0.5, 2), 1)
+
+    def test_divide(self):
+         self.assertEqual(self.calc.divide(8, 2), 4)
+         self.assertEqual(self.calc.divide(6, 2), 3)
+         self.assertEqual(self.calc.divide(5, 2), 2.5)
+         self.assertEqual(self.calc.divide(3, 0), None)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4, 4), 0)
+        self.assertEqual(self.calc.subtract(4, 5), -1)
+        self.assertEqual(self.calc.subtract(4, 0), 4)
+
