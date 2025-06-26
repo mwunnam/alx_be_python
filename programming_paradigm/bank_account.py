db8769d716e8ac2a942bc858a1fcccf104b483a3
mwunnam <mwunnam@gmail.com>
2025-06-26 10:17:19 +0000

    done with task 1

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+
+class BankAccount:
+    """
+    Class for bank account with inital deposit of 0 if none     is give.
+    """
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        """ Deposite method to deposite amount"""
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        """ Withdrawal method for the class """
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        """ Display account method """
+        print(f"Current Balance: {self.account_balance}")
+
